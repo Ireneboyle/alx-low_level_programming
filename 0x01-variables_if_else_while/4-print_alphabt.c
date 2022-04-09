12284271c92d6fe0c3074a4b94afb4b137063b0e
Ireneboyle <81018242+Ireneboyle@users.noreply.github.com>
2022-04-09 02:03:55 +0100

    updated task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,13 +11,12 @@ int main(void)
 
 	while (ch <= 'z')
 	{
- 		if (ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
-		}								
+		}
 		ch++;
 	}
-
 	putchar('\n');
 	return (0);
 }
