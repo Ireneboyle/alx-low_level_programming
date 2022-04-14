cb22cd175ae6ebc6e42d57c2ff7513e8b4980c1a
Ireneboyle <81018242+Ireneboyle@users.noreply.github.com>
2022-04-14 12:48:16 +0100

    Updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *main - main
 *Return: 0
@@ -6,11 +7,10 @@
 
 int main(void)
 {
-int i;
-i=0;
+        int i;
+  
+        i=0;
+        positive_or_negative(i);
 
-positive_or_negative(i);
-
-return (0);
-
+        return (0);
 }
