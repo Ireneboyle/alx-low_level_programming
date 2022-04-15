12648f4afe2a726f5c905dff2b19e5df6ca4e553
Oghomwen Ogbewe <ogbeweoghomwen@yahoo.com>
2022-04-14 17:24:39 -0700

    Updated task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/** 
+ * _isupper - evalue if letter is uppercase
+ *@C: print int
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+if (C >= 'A' && C <= 'Z')
+{
+return (1);
+}
+else
+return (0);
+}
+
+}
